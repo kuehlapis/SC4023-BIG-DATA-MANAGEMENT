f5cbb831df9a3e140a0097304659c5a243b8a325
yaosheng <yaosheng2001@gmail.com>
2026-03-04 18:34:40 +0800

    fix

--- controller/DatabaseController.py
+++ controller/DatabaseController.py
@@ -89,19 +89,20 @@ class DatabaseController:
                 base_query.where(
                     "town",
                     lambda x, towns=valid_towns: str(x).strip().upper() in towns
+                )             
+                base_query.where(
+                    "month_num",
+                    lambda x, start=start_yr_mth: x >= start
                 )
 
+
+
                 for x in range(1, 9):
-                    base_query.where(
-                            "month_num",
-                            lambda x, start=start_yr_mth: x >= start
-                        )
-                    
                     end_month = Helpers.add_months(start_yr_mth, int(x))
+                    base_query.where("month_num", lambda m, end=end_month: m <= end)
+                    
                     for y in range(80, 151):
                         q = base_query.clone()
-
-                        q.where("month_num", lambda m, end=end_month: m <= end)
                         q.where("floor_area_sqm", lambda area, min_sqm=float(y): area >= min_sqm)
 
                         min_psm = q.aggregate("psm_price", "min")
